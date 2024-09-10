a164133d561d2d5a9aff160a60534c4d937153ef
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-10 14:15:47 +0200

    estalished a 12 button grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-
+import Header from "@/components/Header";
 export default function Home() {
-    return (
-        <main>
-
-        </main>
-    );
+  return (
+    <main>
+      <Header />
+      <div className="grid grid-cols-4 gap-4 p-5 max-w-xl mx-auto">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <button
+            key={index}
+            className="bg-gray-100 border-2 border-gray-300 rounded-lg h-28 flex items-center justify-center text-4xl font-bold cursor-pointer transform transition-transform duration-300 hover:bg-gray-200 hover:-translate-y-1"
+          >
+            ?
+          </button>
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from "logo.png";
+const Header = () => {
+  return (
+    <div>
+      <img src="logo.png" alt="test" />
+      <h1>Memorista</h1>
+    </div>
+  );
+};
+
+export default Header;
