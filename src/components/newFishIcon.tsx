ce0af9226bb53b2999565767aed57b7bf0cd6c3d
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-17 19:11:12 +0200

    added a fish spline element

---
+++ src/components/newFishIcon.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function MyFish() {
+  return (
+    <div className="h-[15rem]">
+      <Spline scene="https://prod.spline.design/8swFoh02JxZiVqVr/scene.splinecode" />
+    </div>
+  );
+}
