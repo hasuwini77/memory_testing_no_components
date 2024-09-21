ccf66cbe538e71e4c7e9ff6c06fbbf213462e577
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-21 13:38:39 +0200

    fixed the image header test

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -1,11 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Header from "@/components/Header";
 
-// Mock the MyFish component that uses @splinetool/react-spline
-jest.mock("../components/newFishIcon", () => () => (
-  <div data-testid="mock-fish">Mock Fish Component</div>
-));
-
 describe("Test that the header is bloody awesome", () => {
   test("Check that h1 is rendered correctly", () => {
     render(<Header />);
@@ -20,12 +15,5 @@ describe("Test that the header is bloody awesome", () => {
     const logo = screen.getByRole("img", { name: "logo" });
 
     expect(logo).toBeInTheDocument();
-  });
-
-  test("Check that the MyFish component is rendered", () => {
-    render(<Header />);
-    const fishComponent = screen.getByTestId("mock-fish");
-
-    expect(fishComponent).toBeInTheDocument();
   });
 });
