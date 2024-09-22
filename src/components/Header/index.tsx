530e937e07fd3d0e9d4d11f9f36d1573e67e2653
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-22 17:37:44 +0200

    fixed header with fish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import logo from "/public/logo.png";
 import MyFish from "@/components/newFishIcon";
 
 const Header = () => {
   return (
     <div className="flex flex-col justify-between align-center">
-      <Image src={logo} alt="logo" width={50} height={50} priority hidden />
-      <MyFish />
-      <h1 className="text-center text-2xl bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 text-transparent bg-clip-text [text-shadow:0_0_2px_rgba(0,0,0,0.1)]">
+      <img src="/logo.png" alt="logo" width={100} height={100} />
+      {/* <MyFish /> */}
+      <h1 className="text-center text-4xl p-4 bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 text-transparent bg-clip-text [text-shadow:0_0_2px_rgba(0,0,0,0.1)]">
         Memorista
       </h1>
     </div>
