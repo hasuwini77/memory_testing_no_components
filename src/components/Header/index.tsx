e31a9bcc5cb8ba9c6461cb478a2b7f2b7b64b86d
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-22 17:36:34 +0200

    fixed the rules and header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="flex flex-col justify-between align-center">
       <img src="/logo.png" alt="logo" width={100} height={100} />
       {/* <MyFish /> */}
-      <h1 className="text-center text-2xl bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 text-transparent bg-clip-text [text-shadow:0_0_2px_rgba(0,0,0,0.1)]">
+      <h1 className="text-center text-4xl p-4 bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 text-transparent bg-clip-text [text-shadow:0_0_2px_rgba(0,0,0,0.1)]">
         Memorista
       </h1>
     </div>
--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Rules: React.FC = () => {
   return (
-    <div>
-      <h4>Rules</h4>
-      <p data-testid="rules-text">
+    <div className="bg-purple-500 p-6 rounded-lg max-w-[50rem] mx-auto shadow-lg mb-6">
+      <h4 className="text-2xl font-bold mb-4 text-center">Rules</h4>
+      <p data-testid="rules-text" className="text-md mb-2">
         Click a card to reveal an image. Then click another card to reveal that
         image. Your job is to remember the images and click two of the same
         cards. Good luck beating the highscore!
       </p>
-      <p data-testid="good-luck">Good Luck!</p>
+      <p data-testid="good-luck" className="text-xl font-semibold text-center">
+        Good Luck!
+      </p>
     </div>
   );
 };
